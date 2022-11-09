f2b0ae2ff085cdc822deff196cf9c28f461f4198
samlee088 <108655429+samlee088@users.noreply.github.com>
2022-11-08 19:30:03 -0800

    initial commit

---
+++ models/Category.js
@@ -0,0 +1,31 @@
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection.js');
+
+class Category extends Model {}
+
+Category.init(
+  {
+    // define columns
+    id: {
+      type: DataTypes.Integer,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    id: {
+      type: dataTypes.STRING,
+      allowNull: false
+    }
+
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'category',
+  }
+);
+
+module.exports = Category;
